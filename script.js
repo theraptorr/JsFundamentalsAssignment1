fdcebec29d5fffead6a0618acd8b6c1939b46386
Prince Kumar <129710562+theraptorr@users.noreply.github.com>
2024-03-09 09:13:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let totalValue = 2000;
+let discountPercentage = 20;
+
+// Calculate the discount amount
+let discountAmount = (totalValue * discountPercentage) / 100;
+
+// Calculate the discounted value
+let discountedValue = totalValue - discountAmount;
+
+// Display the discounted value
+
+console.log(`Discounted Value: ${discountedValue}`);
---
+++ script7.js
@@ -0,0 +1,13 @@
+let shoppingList = [
+    "Pen",
+    "Papper",
+    "Candy",
+    "Soda",
+    "Keyboard",
+    "Screen Protector",
+  ];
+  
+  for (let i = 0; i < shoppingList.length; i++) {
+    console.log(shoppingList[i]);
+  }
+  
